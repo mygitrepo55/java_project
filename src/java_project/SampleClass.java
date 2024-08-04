17c87d17897c508789fb1ba873e577a890f6f774
mygitname <vijaychendage55@gmail.com>
2024-08-04 14:04:20 +0530

    first update

---
+++ src/java_project/SampleClass.java
@@ -0,0 +1,11 @@
+package java_project;
+
+public class SampleClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("*************************************");
+		System.out.println("Sample java class for github & jenkin test");
+		System.out.println("*************************************");	
+	}
+}
